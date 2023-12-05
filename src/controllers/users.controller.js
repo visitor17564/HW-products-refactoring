6f0882a64c74a74c031592f9aad4383e7255fa1c
visitor17564 <lanlanloo17564@gmail.com>
2023-12-05 21:39:49 +0900

    [ADD] controller 초안 완성

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,6 +1,61 @@
+import { UsersService } from "../services/users.service.js";
+
 export class UsersController {
-  constructor(usersService) {
-    // 생성자에서 전달받은 PostsService의 의존성을 주입합니다.
-    this.usersService = usersService;
-  }
+  usersService = new UsersService();
+
+  findUsersById = async (req, res, next) => {
+    try {
+      const { id } = res.locals.user;
+
+      // 서비스 계층에 구현된 findUsersById 로직을 실행합니다.
+      const user = await this.usersService.findUsersById(id);
+
+      return res.status(200).json({ data: user });
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  createUser = async (req, res, next) => {
+    try {
+      const { email, password } = req.body;
+
+      if (!email || !password) throw new Error("InvalidParamsError");
+
+      // 서비스 계층에 구현된 createUser 로직을 실행합니다.
+      const createUser = await this.usersService.createUser(email, password);
+
+      return res.status(201).json({ data: createUser });
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  updateUser = async (req, res, next) => {
+    try {
+      const { password } = req.body;
+      const { id } = res.locals.user;
+
+      // 서비스 계층에 구현된 updateUser 로직을 실행합니다.
+      const updateUser = await this.usersService.updateUser(id, password);
+
+      return res.status(200).json({ data: updateUser });
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  deleteUser = async (req, res, next) => {
+    try {
+      const { password } = req.body;
+      const { id } = res.locals.user;
+
+      // 서비스 계층에 구현된 deleteUser 로직을 실행합니다.
+      const deleteUser = await this.usersService.deleteUser(postId);
+
+      return res.status(200).json({ data: deleteUser });
+    } catch (err) {
+      next(err);
+    }
+  };
 }
