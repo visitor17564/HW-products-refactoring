7663d54e4677aa6d318bd3b5ac58a576ee5c7f53
visitor17564 <lanlanloo17564@gmail.com>
2023-12-05 21:56:23 +0900

    [ADD] router 초안 완성

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -12,4 +12,19 @@ const productsRepository = new ProductsRepository(prisma);
 const productsService = new ProductsService(productsRepository);
 const productsController = new ProductsController(productsService);
 
+/** 상품 조회 API **/
+router.get("/", productsController.findAllProducts);
+
+/** 상품 상세 조회 API **/
+router.get("/:productId", productsController.findProductById);
+
+/** 상품 작성 API **/
+router.post("/", productsController.createProduct);
+
+/** 상품 수정 API **/
+router.put("/:productId", productsController.updateProduct);
+
+/** 상품 삭제 API **/
+router.delete("/:productId", productsController.deleteProduct);
+
 export default router;
