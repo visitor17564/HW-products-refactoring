bc0477b4551104c4ef90f656932e61274463b9c3
visitor17564 <lanlanloo17564@gmail.com>
2023-12-05 21:18:06 +0900

    [ADD] 서비스 초안 작성 완료

--- src/services/users.service.js
+++ src/services/users.service.js
@@ -1,6 +1,69 @@
+import { UsersRepository } from "../repositories/users.repository.js";
 export class UsersService {
-  constructor(usersRepository) {
-    // 생성자에서 전달받은 PostsRepository 의존성을 주입합니다.
-    this.usersRepository = usersRepository;
-  }
+  usersRepository = new UsersRepository();
+
+  findUsersById = async (id) => {
+    // 저장소(Repository)에게 특정 게시글 하나를 요청합니다.
+    const user = await this.usersRepository.findUsersById(id);
+
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt
+    };
+  };
+
+  createUser = async (email, password) => {
+    // 저장소(Repository)에게 데이터를 요청합니다.
+    const createUser = await this.usersRepository.createUser(email, password);
+
+    // 비즈니스 로직을 수행한 후 사용자에게 보여줄 데이터를 가공합니다.
+    return {
+      id: createUser.id,
+      name: createUser.name,
+      email: createUser.email,
+      createdAt: createUser.createdAt,
+      updatedAt: createUser.updatedAt
+    };
+  };
+
+  updateUser = async (id, password) => {
+    // 저장소(Repository)에게 특정 게시글 하나를 요청합니다.
+    const user = await this.usersRepository.findUsersById(id);
+    if (!user) throw new Error("존재하지 않는 사용자입니다.");
+
+    // 저장소(Repository)에게 데이터 수정을 요청합니다.
+    await this.usersRepository.updateUser(id, password);
+
+    // 변경된 데이터를 조회합니다.
+    const updateUser = await this.usersRepository.findUsersById(id);
+
+    return {
+      id: updateUser.id,
+      name: updateUser.name,
+      email: updateUser.email,
+      password: updateUser.password,
+      createdAt: updateUser.createdAt,
+      updatedAt: updateUser.updatedAt
+    };
+  };
+
+  deleteUser = async (id, password) => {
+    // 저장소(Repository)에게 특정 게시글 하나를 요청합니다.
+    const user = await this.usersRepository.findUsersById(id);
+    if (!user) throw new Error("존재하지 않는 게시글입니다.");
+
+    // 저장소(Repository)에게 데이터 삭제를 요청합니다.
+    await this.productsRepository.deleteUser(id, password);
+
+    return {
+      id: updateUser.id,
+      name: updateUser.name,
+      email: updateUser.email,
+      createdAt: updateUser.createdAt,
+      updatedAt: updateUser.updatedAt
+    };
+  };
 }
